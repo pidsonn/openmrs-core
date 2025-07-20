f4a9ef920d56e1000db267b152a15dd602bd6565
Muturane Pidson <muturanepidson@gmail.com>
2025-07-20 13:07:31 +0300

    TRUNK-247: Add required value constructors and corresponding tests

--- api/src/main/java/org/openmrs/User.java
+++ api/src/main/java/org/openmrs/User.java
@@ -68,11 +68,11 @@ import org.slf4j.LoggerFactory;
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Audited
 public class User extends BaseOpenmrsObject implements java.io.Serializable, Attributable<User>, Auditable, Retireable {
-	
+
 	public static final long serialVersionUID = 2L ;
-	
+
 	private static final Logger log = LoggerFactory.getLogger(User.class);
-	
+
 	// Fields
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_user_id_seq")
@@ -146,76 +146,88 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 
 	@Column(name = "retire_reason", length = 255)
 	private String retireReason;
-	
+
 	// Constructors
-	
+
 	/** default constructor */
 	public User() {
 	}
-	
+
 	/** constructor with id */
 	public User(Integer userId) {
 		this.userId = userId;
 	}
-	
+
 	/** constructor with person object */
 	public User(Person person) {
 		this.person = person;
 	}
-	
+
+	/**
+    * Required values constructor
+    * for saving a valid User to the database.
+    */
+    public User(Person person, String systemId, User creator, Date dateCreated, boolean retired) {
+        this.person = person;
+        this.systemId = systemId;
+        this.creator = creator;
+        this.dateCreated = dateCreated;
+        this.retired = retired;
+    }
+
 	/**
 	 * Return true if this user has all privileges
-	 * 
+	 *
 	 * @return true/false if this user is defined as a super user
 	 */
 	public boolean isSuperUser() {
 		return containsRole(RoleConstants.SUPERUSER);
 	}
-	
+
 	/**
 	 * This method shouldn't be used directly. Use org.openmrs.api.context.Context#hasPrivilege so that
 	 * anonymous/authenticated/proxy privileges are all included Return true if this user has the
 	 * specified privilege
-	 * 
+	 *
 	 * @param privilege
 	 * @return true/false depending on whether user has specified privilege
 	 */
 	public boolean hasPrivilege(String privilege) {
-		
+
 		// All authenticated users have the "" (empty) privilege
 		if (StringUtils.isEmpty(privilege)) {
 			return true;
 		}
-		
+
 		if (isSuperUser()) {
 			return true;
 		}
-		
+
 		Set<Role> tmproles = getAllRoles();
-		
+
 		// loop over the roles and check each for the privilege
 		for (Role tmprole : tmproles) {
 			if (tmprole.hasPrivilege(privilege)) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Check if this user has the given String role
-	 * 
+	 *
 	 * @param r String name of a role to check
 	 * @return Returns true if this user has the specified role, false otherwise
 	 */
 	public boolean hasRole(String r) {
 		return hasRole(r, false);
 	}
-	
+
 	/**
 	 * Checks if this user has the given String role
-	 * 
+	 *
 	 * @param r String name of a role to check
 	 * @param ignoreSuperUser If this is false, then this method will always return true for a
 	 *            superuser.
@@ -226,21 +238,21 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		if (!ignoreSuperUser && isSuperUser()) {
 			return true;
 		}
-		
+
 		if (roles == null) {
 			return false;
 		}
-		
+
 		Set<Role> tmproles = getAllRoles();
-		
+
 		log.debug("User # {} has roles: {}", userId, tmproles);
-		
+
 		return containsRole(r);
 	}
-	
+
 	/**
 	 * Checks if the user has a given role. Role name comparisons are not case sensitive.
-	 * 
+	 *
 	 * @param roleName the name of the role to check
 	 * @return true if the user has the given role, else false
 	 * <strong>Should</strong> return true if the user has the given role
@@ -255,17 +267,17 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Get <i>all</i> privileges this user has. This delves into all of the roles that a person has,
 	 * appending unique privileges
-	 * 
+	 *
 	 * @return Collection of complete Privileges this user has
 	 */
 	public Collection<Privilege> getPrivileges() {
 		Set<Privilege> privileges = new HashSet<>();
 		Set<Role> tmproles = getAllRoles();
-		
+
 		Role role;
 		for (Role tmprole : tmproles) {
 			role = tmprole;
@@ -274,22 +286,22 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 				privileges.addAll(privs);
 			}
 		}
-		
+
 		return privileges;
 	}
-	
+
 	// Property accessors
-	
+
 	/**
 	 * Returns all roles attributed to this user by expanding the role list to include the parents of
 	 * the assigned roles
-	 * 
+	 *
 	 * @return all roles (inherited from parents and given) for this user
 	 */
 	public Set<Role> getAllRoles() {
 		// the user's immediate roles
 		Set<Role> baseRoles = new HashSet<>();
-		
+
 		// the user's complete list of roles including
 		// the parent roles of their immediate roles
 		Set<Role> totalRoles = new HashSet<>();
@@ -297,9 +309,9 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 			baseRoles.addAll(getRoles());
 			totalRoles.addAll(getRoles());
 		}
-		
+
 		log.debug("User's base roles: {}", baseRoles);
-		
+
 		try {
 			for (Role r : baseRoles) {
 				totalRoles.addAll(r.getAllParentRoles());
@@ -315,24 +327,24 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		}
 		return totalRoles;
 	}
-	
+
 	/**
 	 * @return Returns the roles.
 	 */
 	public Set<Role> getRoles() {
 		return roles;
 	}
-	
+
 	/**
 	 * @param roles The roles to set.
 	 */
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
-	
+
 	/**
 	 * Add the given Role to the list of roles for this User
-	 * 
+	 *
 	 * @param role
 	 * @return Returns this user with the given role attached
 	 */
@@ -343,13 +355,13 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		if (!roles.contains(role) && role != null) {
 			roles.add(role);
 		}
-		
+
 		return this;
 	}
-	
+
 	/**
 	 * Remove the given Role from the list of roles for this User
-	 * 
+	 *
 	 * @param role
 	 * @return this user with the given role removed
 	 */
@@ -357,10 +369,10 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		if (roles != null) {
 			roles.remove(role);
 		}
-		
+
 		return this;
 	}
-	
+
 	/**
 	 * @see org.openmrs.Attributable#findPossibleValues(java.lang.String)
 	 */
@@ -374,7 +386,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 			return Collections.emptyList();
 		}
 	}
-	
+
 	/**
 	 * @see org.openmrs.Attributable#getPossibleValues()
 	 */
@@ -388,7 +400,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 			return Collections.emptyList();
 		}
 	}
-	
+
 	/**
 	 * @see org.openmrs.Attributable#hydrate(java.lang.String)
 	 */
@@ -401,7 +413,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 			return new User();
 		}
 	}
-	
+
 	/**
 	 * @see org.openmrs.Attributable#serialize()
 	 */
@@ -413,7 +425,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 			return "";
 		}
 	}
-	
+
 	/**
 	 * @see org.openmrs.Attributable#getDisplayString()
 	 */
@@ -423,40 +435,40 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		if (getPersonName() != null) {
 			returnString += getPersonName().getFullName() + " ";
 		}
-		
+
 		returnString += "(" + getUsername() + ")";
 		return returnString;
-		
+
 	}
-	
+
 	/**
 	 * @return Returns the systemId.
 	 */
 	public String getSystemId() {
 		return systemId;
 	}
-	
+
 	/**
 	 * @param systemId The systemId to set.
 	 */
 	public void setSystemId(String systemId) {
 		this.systemId = systemId;
 	}
-	
+
 	/**
 	 * @return Returns the userId.
 	 */
 	public Integer getUserId() {
 		return userId;
 	}
-	
+
 	/**
 	 * @param userId The userId to set.
 	 */
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
-	
+
 	/**
 	 * @return the person
 	 * @since 1.6
@@ -464,7 +476,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public Person getPerson() {
 		return person;
 	}
-	
+
 	/**
 	 * @return the person, creating a new object if person is null
 	 */
@@ -474,7 +486,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		}
 		return person;
 	}
-	
+
 	/**
 	 * @param person the person to set
 	 * @since 1.6
@@ -482,21 +494,21 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public void setPerson(Person person) {
 		this.person = person;
 	}
-	
+
 	/**
 	 * @return Returns the username.
 	 */
 	public String getUsername() {
 		return username;
 	}
-	
+
 	/**
 	 * @param username The username to set.
 	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	/**
 	 * @since 2.2
 	 * @return Returns the email.
@@ -504,7 +516,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public String getEmail() {
 		return email;
 	}
-	
+
 	/**
 	 * @since 2.2
 	 * @param email The email to set.
@@ -512,12 +524,12 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	@Override
 	public String toString() {
 		return StringUtils.isNotBlank(username) ? username : systemId;
 	}
-	
+
 	/**
 	 * @return Returns the userProperties.
 	 */
@@ -527,21 +539,21 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		}
 		return userProperties;
 	}
-	
+
 	/**
 	 * @param userProperties A Map&lt;String,String&gt; of the properties to set.
 	 */
 	public void setUserProperties(Map<String, String> userProperties) {
 		this.userProperties = userProperties;
 	}
-	
+
 	/**
 	 * Convenience method. Adds the given property to the user's properties
 	 */
 	public void setUserProperty(String prop, String value) {
 		getUserProperties().put(prop, value);
 	}
-	
+
 	/**
 	 * Convenience method. Removes the given property from the user's properties
 	 */
@@ -550,11 +562,11 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 			userProperties.remove(prop);
 		}
 	}
-	
+
 	/**
 	 * Get prop property from this user's properties. If prop is not found in properties, return empty
 	 * string
-	 * 
+	 *
 	 * @param prop
 	 * @return property value
 	 */
@@ -562,14 +574,14 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		if (getUserProperties() != null && userProperties.containsKey(prop)) {
 			return userProperties.get(prop);
 		}
-		
+
 		return "";
 	}
-	
+
 	/**
 	 * Get prop property from this user's properties. If prop is not found in properties, return
 	 * <code>defaultValue</code>
-	 * 
+	 *
 	 * @param prop
 	 * @param defaultValue
 	 * @return property value
@@ -579,57 +591,57 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 		if (getUserProperties() != null && userProperties.containsKey(prop)) {
 			return userProperties.get(prop);
 		}
-		
+
 		return defaultValue;
 	}
-	
+
 	/**
 	 * @see Person#addName(PersonName)
 	 */
 	public void addName(PersonName name) {
 		getPersonMaybeCreate().addName(name);
 	}
-	
+
 	/**
 	 * @see Person#getPersonName()
 	 */
 	public PersonName getPersonName() {
 		return getPerson() == null ? null : getPerson().getPersonName();
 	}
-	
+
 	/**
 	 * Get givenName on the Person this user account belongs to
-	 * 
+	 *
 	 * @see Person#getGivenName()
 	 */
 	public String getGivenName() {
 		return getPerson() == null ? null : getPerson().getGivenName();
 	}
-	
+
 	/**
 	 * Get familyName on the Person this user account belongs to
-	 * 
+	 *
 	 * @see Person#getFamilyName()
 	 */
 	public String getFamilyName() {
 		return getPerson() == null ? null : getPerson().getFamilyName();
 	}
-	
+
 	/**
 	 * @see org.openmrs.Person#getNames()
 	 */
 	public Set<PersonName> getNames() {
 		return person.getNames();
 	}
-	
+
 	/**
 	 * Returns a list of Locales for which the User is considered proficient.
-	 * 
+	 *
 	 * @return List of the User's proficient locales
 	 */
 	public List<Locale> getProficientLocales() {
 		String proficientLocalesProperty = getUserProperty(OpenmrsConstants.USER_PROPERTY_PROFICIENT_LOCALES);
-		
+
 		if ((proficientLocales == null)
 		        || (!OpenmrsUtil.nullSafeEquals(parsedProficientLocalesProperty, proficientLocalesProperty))) {
 			parsedProficientLocalesProperty = proficientLocalesProperty;
@@ -653,11 +665,11 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 				}
 			}
 		}
-		
+
 		// return a copy so that the list isn't changed by other processes
 		return new ArrayList<>(proficientLocales);
 	}
-	
+
 	/**
 	 * @since 1.5
 	 * @see org.openmrs.OpenmrsObject#getId()
@@ -666,7 +678,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public Integer getId() {
 		return getUserId();
 	}
-	
+
 	/**
 	 * @since 1.5
 	 * @see org.openmrs.OpenmrsObject#setId(java.lang.Integer)
@@ -675,7 +687,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public void setId(Integer id) {
 		setUserId(id);
 	}
-	
+
 	@Override
 	public User getCreator() {
 		return creator;
@@ -685,7 +697,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public void setCreator(User creator) {
 		this.creator = creator;
 	}
-	
+
     @Override
 	public Date getDateCreated() {
 		return dateCreated;
@@ -720,7 +732,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	public Boolean isRetired() {
 		return retired;
 	}
-	
+
     @Override
 	public Boolean getRetired() {
 		return retired;
