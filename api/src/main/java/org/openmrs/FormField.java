ba48e051caa5b213cbd2117897bbecc69ea88935
Muturane Pidson <muturanepidson@gmail.com>
2025-08-17 17:29:53 +0300

    TRUNK-5822: Update FormField entity and pom.xml to use jakarta.persistence

--- api/src/main/java/org/openmrs/FormField.java
+++ api/src/main/java/org/openmrs/FormField.java
@@ -12,14 +12,14 @@ package org.openmrs;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.hibernate.envers.Audited;
